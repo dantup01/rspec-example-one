d50cf887187f516df95f4222276059ed01cbe915
Daniel Tuppen <daniel.tuppen1@gmail.com>
2021-04-27 19:43:33 +0100

    created rspec test

---
+++ calculator.rb
@@ -0,0 +1,23 @@
+require "rspec/autorun"
+
+class Calculator
+  def add(a, b)
+    return a + b
+  end
+end
+
+describe Calculator do
+  describe "#add" do
+    it "returns the sum of its two arguments" do
+      calc = Calculator.new
+
+      expect(calc.add(5, 10)).to eq(15)
+    end
+
+    it "returns the sum of two different arguments" do
+      calc = Calculator.new
+
+      expect(calc.add(1, 2)).to eq(3)
+    end
+  end
+end
