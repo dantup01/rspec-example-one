dcad4018cf5f8b2ed12686f2586ce1d8f76c445e
Daniel Tuppen <daniel.tuppen1@gmail.com>
2021-04-27 19:50:09 +0100

    updated tests

--- calculator.rb
+++ calculator.rb
@@ -4,6 +4,14 @@ class Calculator
   def add(a, b)
     return a + b
   end
+
+  def factorial(n)
+    if n < 1
+      1
+    else
+      n * factorial(n-1)
+    end
+  end
 end
 
 describe Calculator do
@@ -18,6 +26,20 @@ describe Calculator do
       calc = Calculator.new
 
       expect(calc.add(1, 2)).to eq(3)
+    end
+  end
+
+  describe "#factorial" do
+    it "returns 1 when given 0" do
+      calc = Calculator.new
+
+      expect(calc.factorial(0)).to eq(1)
+    end
+
+    it "returns 120 when given 5" do
+      calc = Calculator.new
+
+      expect(calc.factorial(5)).to eq(120)
     end
   end
 end
